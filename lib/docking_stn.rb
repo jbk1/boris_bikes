c04d97836ad439e1c3af0f7f5dbdacb3a02b8f06
James B Kemp <james@bibble.com>
2014-05-29 11:39:58 +0100

    completed docking_stn capacity, all passing

--- lib/docking_stn.rb
+++ lib/docking_stn.rb
@@ -3,28 +3,31 @@ class DockingStation
 	DEFAULT_CAPACITY = 10
 
 	def initialize(options = {})
-		
 		@capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
 		@bikes = []
 	end
+
 
 	def bike_count
 		@bikes.count
 	end
 
+
 	def dock(bike)
 		@bikes << bike
 	end
+
 
 	def release(bike)
 		@bikes.delete(bike)
 	end
 
+
 	def not_to_be_full
 	end
 
 	def full?
-		bike.count == @capacity
+		bike_count == @capacity
 	end
 
 
